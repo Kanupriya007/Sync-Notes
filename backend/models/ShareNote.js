44bd3f71349a8e86959bef4def15f37237b3f4a0
rajraghuwanshi007 <rajraghuwanshi393@gmail.com>
2023-08-13 23:44:48 +0530

    friends sharing added along with request for friends

--- backend/models/ShareNote.js
+++ backend/models/ShareNote.js
@@ -1,34 +1,36 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
+const notesSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  title: {
     type: String,
     required: true,
   },
-  email: {
+  description: {
     type: String,
     required: true,
-    unique: true,
   },
-  password: {
+  tag: {
     type: String,
+    default: "General",
+  },
+  to:{
+    type:String,
+    required: true,
+  },
+  from:{
+    type:String,
     required: true,
   },
   date: {
     type: Date,
     default: Date.now,
   },
-  sent: {
-    type: Array,
-  },
-  recieve: {
-    type: Array,
-  },
-  friends: {
-    type: Array,
-  },
 });
 
 // Mongoose automatically looks for the plural, lowercased version of your model name so user will be for users collections in db.
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("shared", notesSchema);
